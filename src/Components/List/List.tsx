622c1efdb801624b915e51ca65395aeb49ef4b21
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-05-29 16:13:34 -0400

    Try creating list items

--- src/Components/List/List.tsx
+++ src/Components/List/List.tsx
@@ -1,1 +1,16 @@
+// import React from 'react'
+// import '.List.css'
+
+// const List = ( list: {} ) => {
+//
+//   const listItems = list.map(item => {
+//     return (
+//       // <li className='listItem' id={item.list_name_encoded} >{item.display_name}</li>
+//       <h1>FML {item}</h1>
+//     )
+//   })
+//   return <div></div>
+// }
+//
+// export default List
 export {}
