9e072aa8ab074763e1d49e1294df810f1b3500c4
schlanjo <schlanjo@gmail.com>
2021-05-29 11:24:17 -0600

    Finish initial design and implementation of Navbar

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,7 +1,13 @@
+import './Navbar.css'
+
 function Navbar() {
   return (
-    <nav>
+    <nav className='nav'>
       <h1>BookWorm</h1>
+      <ul>
+        <li>Home</li>
+        <li>Favorites</li>
+      </ul>
     </nav>
   )
 }
