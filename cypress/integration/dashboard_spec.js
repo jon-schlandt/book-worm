652f9184b40ded68a372865a6d584b9968461577
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-05-30 15:31:07 -0400

    Add cypress testing for dashboard with load command

---
+++ cypress/integration/dashboard_spec.js
@@ -0,0 +1,31 @@
+describe('Details', () => {
+    beforeEach(() => {
+      cy.load()
+    });
+    it('Should show NavBar', () => {
+      cy.get('h2').should('be.visible')
+        .get('h2').contains('Loading...')
+        .get('.nav').should('be.visible')
+        .get('.nav>h1').contains('BookWorm')
+        .get('.nav>ul>li').should('be.visible')
+        .get('.nav>ul>li').eq(0).should('contain', 'Home')
+        .get('.nav>ul>li').eq(1).should('contain', 'Home')
+        .get('.nav').contains('Home')
+        .get('.nav').contains('Favorites')
+        .get('h2').should('not.exist')
+    })
+  
+    it('Should show list of book types', () => {
+      cy.get('h2').should('be.visible')
+        .get('h2').contains('Loading...')
+        .get('ul').should('be.visible')
+        .get('ul>li').eq(1)
+        .get('.listItem').should('be.visible')
+        cy.get('.listItem').eq(0).should('contain', 'Combined Print & E-Book Fiction')
+        cy.get('.listItem').eq(1).should('contain', 'Combined Print & E-Book Nonfiction')
+        // .get('.listItem').contains('Combined Print & E-Book Fiction')
+        // .get('.listItem').contains('Combined Print & E-Book Nonfiction')
+        .get('h2').should('not.exist')
+    })
+});
+  
