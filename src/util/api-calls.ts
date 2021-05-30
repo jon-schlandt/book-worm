30c127087b5f12a4dda89694f8614d32688c41f7
schlanjo <schlanjo@gmail.com>
2021-05-30 17:02:43 -0600

    Add fetch endpoint back, remove error test

--- src/util/api-calls.ts
+++ src/util/api-calls.ts
@@ -3,7 +3,7 @@ import {cleanListData} from './utilities'
 // ***** ----- Fetching ----- ***** //
 
 export const getLists = () => {
-  return fetch('http://httpstat.us/416')
+  return fetch('https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=A7HbLAtK8PlqUjAQ0Ol77w3tNU1cZS4b')
     .then(response => {
       if (response.ok) {
         return response.json()
