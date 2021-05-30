47a563f096866be407aab579f8cd977e7242e329
schlanjo <schlanjo@gmail.com>
2021-05-30 15:51:57 -0600

    Add error testing endpoint, check for ok response in getLists()

--- src/util/api-calls.ts
+++ src/util/api-calls.ts
@@ -2,10 +2,20 @@ import {cleanListData} from './utilities'
 
 // Maybe getCategoryOf and error handling?
 
+const listEndpoint = 'https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=A7HbLAtK8PlqUjAQ0Ol77w3tNU1cZS4b'
+const errorEndpoint = 'https://httpstat.us/404'
+
 export const getLists = () => {
-  return fetch('https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=A7HbLAtK8PlqUjAQ0Ol77w3tNU1cZS4b')
-    .then(response => response.json())
+  return fetch(errorEndpoint)
+    .then(response => {
+      if (response.ok) {
+        return response.json()
+      }
+
+      console.log(response)
+    })
     .then(data => cleanListData(data.results))
+    .catch(error => console.log(error))
 }
 
 export const getTypeOf = ( typeOf: string ) => {
