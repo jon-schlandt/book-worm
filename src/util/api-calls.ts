aaedd0e5d2ca69ccfaaf0c8934e6fb6208867c3e
schlanjo <schlanjo@gmail.com>
2021-05-30 16:13:03 -0600

    Build out function for handling errors conditionally

--- src/util/api-calls.ts
+++ src/util/api-calls.ts
@@ -3,7 +3,9 @@ import {cleanListData} from './utilities'
 // Maybe getCategoryOf and error handling?
 
 const listEndpoint = 'https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=A7HbLAtK8PlqUjAQ0Ol77w3tNU1cZS4b'
-const errorEndpoint = 'https://httpstat.us/404'
+const errorEndpoint = 'https://httpstat.us/500'
+
+// ***** ----- Fetching ----- ***** //
 
 export const getLists = () => {
   return fetch(errorEndpoint)
@@ -12,13 +14,26 @@ export const getLists = () => {
         return response.json()
       }
 
-      console.log(response)
+      handleError(response.status)
     })
     .then(data => cleanListData(data.results))
-    .catch(error => console.log(error))
 }
 
 export const getTypeOf = ( typeOf: string ) => {
   return fetch(`https://api.nytimes.com/svc/books/v3/lists/current/${typeOf}.json?api-key=A7HbLAtK8PlqUjAQ0Ol77w3tNU1cZS4b`)
     .then(response => response.json())
+}
+
+// ***** ----- Error Handling ----- ***** //
+
+function handleError(status: number) {
+  if (status === 404) {
+    throw Error('404')
+  }
+
+  if (status === 500) {
+    throw Error('500')
+  }
+
+  throw Error('Sorry, something went wrong.')
 }
