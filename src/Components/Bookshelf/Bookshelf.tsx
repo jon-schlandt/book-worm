96b32a9a8e249a5fb79c0f80b6bb684aee9c3b4d
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-05-31 15:16:48 -0400

    Set type of BookshelfState to consoleLog this.state.books

--- src/Components/Bookshelf/Bookshelf.tsx
+++ src/Components/Bookshelf/Bookshelf.tsx
@@ -1,16 +1,36 @@
 import React from 'react'
+import { getTypeOf } from '../../util/api-calls'
 import '../Book/Book'
 
 type BookshelfProps = {
   queryID: string
 }
 
-class Bookshelf extends React.Component<BookshelfProps> {
+type BookshelfState = {
+    books: {
+    rank: number,
+    publisher: string,
+    description: string,
+    title: string,
+    author: string,
+    bookImage: string,
+    amazonProductUrl: string
+  }[] | []
+}
+
+class Bookshelf extends React.Component<BookshelfProps, BookshelfState> {
   constructor(props: BookshelfProps) {
     super(props)
     this.state= {
-
+      books: [],
     }
+  }
+
+  componentDidMount() {
+    getTypeOf( this.props.queryID )
+      .then(result => this.setState({books: result}, () =>  console.log("RESULT", this.state.books)))
+      console.log("STATE", this.state.books);
+      
   }
 
   render() {
