99174ac41064fd3c01e7ee4de33b96a23a52de1d
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-05-31 16:35:48 -0400

    Build out book card structure by creating type BookProps

--- src/Components/Book/Book.tsx
+++ src/Components/Book/Book.tsx
@@ -1,1 +1,21 @@
-export {}
+import './Book.css'
+
+type BookProps = {
+    title: string,
+    author: string,
+    rank: number,
+    bookImage: string,
+}
+
+function Book({title, author, rank, bookImage}: BookProps) {
+    return (
+        <article className='bookCard'>
+            <img className='img' src={bookImage} alt='Book Cover'/>
+            <h2 className='title'>{title}</h2>
+            <h3 className='author'>{author}</h3>
+            <p className='rank'>Rank: {rank}</p>
+        </article>
+)
+}
+
+export default Book
