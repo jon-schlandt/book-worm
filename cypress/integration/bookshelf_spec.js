114ed1d2986677bfb2ff10c5862993eb07dc84ea
Sarah Lane <sarahlane8@gmail.com>
2021-05-31 17:14:01 -0600

    Add bookcards test

--- cypress/integration/bookshelf_spec.js
+++ cypress/integration/bookshelf_spec.js
@@ -10,5 +10,15 @@ describe('BookshelfDisplay', () => {
     .get('.nav-links').should('contain', 'Favorites')
   })
 
-  
+  it('Should display bookcards based on a genre', () => {
+    cy.get('.bookshelf').should('be.visible')
+      .get('.bookCard').should('have.length', 2)
+      .get('.bookshelf > article').eq(0).should('contain', 'THE LAST THING HE TOLD ME')
+        .should('contain', 'Laura Dave')
+        .should('contain', 'Rank: 1')
+      .get('.bookshelf > article').eq(1).should('contain', 'SOOLEY')
+        .should('contain', 'John Grisham')
+        .should('contain', 'Rank: 2')
+  })
+
 })
