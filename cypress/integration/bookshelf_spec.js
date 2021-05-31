2f8bf856b880896637530dc9d48722822c46c116
Sarah Lane <sarahlane8@gmail.com>
2021-05-31 17:27:08 -0600

    Complete testing to bookshelf display without error handling

--- cypress/integration/bookshelf_spec.js
+++ cypress/integration/bookshelf_spec.js
@@ -5,9 +5,10 @@ describe('BookshelfDisplay', () => {
   })
 
   it('Should display the navbar', () => {
-    cy.get('.nav').should('be.visible').should('contain', 'BookWorm')
-    .get('.nav-links').should('contain', 'Home')
-    .get('.nav-links').should('contain', 'Favorites')
+    cy.get('.nav').should('be.visible')
+      .get('nav > h1').should('contain', 'BookWorm')
+      .get('nav > ul > li').eq(0).should('contain', 'Home')
+      .get('nav > ul > li').should('contain', 'Favorites')
   })
 
   it('Should display bookcards based on a genre', () => {
@@ -19,6 +20,10 @@ describe('BookshelfDisplay', () => {
       .get('.bookshelf > article').eq(1).should('contain', 'SOOLEY')
         .should('contain', 'John Grisham')
         .should('contain', 'Rank: 2')
+  })
+
+  it('Should include the queryName in the URL', () => {
+     cy.url().should('eq', 'http://localhost:3000/bookshelf/combined-print-and-e-book-fiction')
   })
 
 })
