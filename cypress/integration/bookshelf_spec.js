a6e5a69d9c28e7d81e4f0acbbc59d2ae6624fcda
Sarah Lane <sarahlane8@gmail.com>
2021-05-31 16:57:28 -0600

    Complete navbar display test

--- cypress/integration/bookshelf_spec.js
+++ cypress/integration/bookshelf_spec.js
@@ -1,9 +1,14 @@
 describe('BookshelfDisplay', () => {
   beforeEach( () => {
+    cy.load()
     cy.bookshelf()
   })
 
   it('Should display the navbar', () => {
-    cy.get('.nav').should('be.visible')
+    cy.get('.nav').should('be.visible').should('contain', 'BookWorm')
+    .get('.nav-links').should('contain', 'Home')
+    .get('.nav-links').should('contain', 'Favorites')
   })
+
+  
 })
