c7c1f2889159c8775d52f8412f3a89b4349578a5
Sarah Lane <sarahlane8@gmail.com>
2021-06-01 10:09:23 -0600

    Add router to Favorites button with active styling

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -2,22 +2,29 @@ import './Navbar.css'
 import { NavLink } from 'react-router-dom'
 
 function Navbar() {
+  const activeStyling= {
+    textDecoration: 'underline',
+    textDecorationColor: '#F5F1E1',
+    color: 'inherit'
+  }
+
   return (
     <nav className='nav'>
       <h1>BookWorm</h1>
       <ul className='nav-links'>
         <NavLink
           exact to='/'
-          activeStyle={{
-            textDecoration: 'underline',
-            textDecorationColor: '#F5F1E1',
-            color: 'inherit'
-          }}
+          activeStyle={activeStyling}
           >
           <li>Home</li>
         </NavLink>
 
+        <NavLink
+          exact to='/bookshelf/favorites'
+          activeStyle={activeStyling}
+          >
           <li>Favorites</li>
+        </NavLink>
       </ul>
     </nav>
   )
