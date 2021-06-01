cdfcf5d2f03e2b92a6357121d9c42796a097e203
Sarah Lane <sarahlane8@gmail.com>
2021-06-01 10:04:43 -0600

    Add NavLink for Home button with CSS styling for active

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,12 +1,23 @@
 import './Navbar.css'
+import { NavLink } from 'react-router-dom'
 
 function Navbar() {
   return (
     <nav className='nav'>
       <h1>BookWorm</h1>
       <ul className='nav-links'>
-        <li>Home</li>
-        <li>Favorites</li>
+        <NavLink
+          exact to='/'
+          activeStyle={{
+            textDecoration: 'underline',
+            textDecorationColor: '#F5F1E1',
+            color: 'inherit'
+          }}
+          >
+          <li>Home</li>
+        </NavLink>
+
+          <li>Favorites</li>
       </ul>
     </nav>
   )
