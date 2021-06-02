ad48934821348ff7ae0bf4069e1dbb5e34103732
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-06-02 12:18:19 -0400

    Place rank & favorite button at top of the container

--- src/Components/Book/Book.tsx
+++ src/Components/Book/Book.tsx
@@ -12,13 +12,15 @@ type BookProps = {
 function Book({ title, author, rank, bookImage, handleClick }: BookProps) {
     return (
         <article className='bookCard'>
+          <span>
+            <p className='rank'>Rank: {rank}</p>
+            <button onClick={ () => handleClick(title)}>Add to Favorites 🐛</button>
+          </span>
+          <div className='bookDetails'>
             <img className='img' src={bookImage} alt='Book Cover'/>
             <h2 className='title'>{title}</h2>
             <h3 className='author'>{author}</h3>
-            <span>
-              <p className='rank'>Rank: {rank}</p>
-              <button onClick={ () => handleClick(title)}>Add to Favorites 🐛</button>
-            </span>
+          </div>
         </article>
       )
 }
