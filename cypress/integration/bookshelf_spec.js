df0d5a8a8ef911b31bfd0eb1c081766232d4a83e
schlanjo <schlanjo@gmail.com>
2021-06-01 23:00:55 -0600

    Add testing for verifying favorite persistence for single session

--- cypress/integration/bookshelf_spec.js
+++ cypress/integration/bookshelf_spec.js
@@ -50,7 +50,7 @@ describe('Favorite button', () => {
   })
 
   it('should only add a book to Favorites if it has not already been added', () => {
-    cy.get('.favoritesBtn').eq(0).should('be.visible')
+    cy.get('.favoritesBtn').eq(0)
       .click()
       .click()
 
@@ -60,5 +60,31 @@ describe('Favorite button', () => {
     cy.url().should('eq', 'http://localhost:3000/bookshelf/favorites')
       .get('.bookshelf').should('be.visible')
       .get('.bookCard').should('have.length', 1)
+  })
+})
+
+describe('Favorites display', () => {
+  beforeEach(() => {
+    cy.load()
+    cy.bookshelf()
+  })
+
+  it('should continue to hold current session favorites after returning Home', () => {
+    cy.get('.favoritesBtn').eq(0)
+      .click()
+    
+    cy.get('nav > ul > a').eq(0)
+      .click()
+
+    cy.url().should('eq', 'http://localhost:3000/')
+      .get('nav > ul > a').eq(1)
+      .click()
+    
+    cy.url().should('eq', 'http://localhost:3000/bookshelf/favorites')
+      .get('.bookshelf').should('be.visible')
+      .get('.bookCard').should('have.length', 1)
+      .get('.bookshelf > article').eq(0).should('contain', 'THE LAST THING HE TOLD ME')
+        .should('contain', 'Laura Dave')
+        .should('contain', 'Rank: 1')
   })
 })
