79860e5f1f36ddac82fcbb7bf02407046af0db8d
Sarah Lane <sarahlane8@gmail.com>
2021-06-03 13:54:21 -0600

    Refactor should only add a book to Favorites if it has not alreayd been adeed test to check for what the bookcard contains

--- cypress/integration/favorites_spec.js
+++ cypress/integration/favorites_spec.js
@@ -7,7 +7,7 @@ describe('Favorite button', () => {
   it('should add its linked book to the Favorites bookshelf', () => {
     cy.get('.favoritesBtn').eq(0).should('be.visible')
       .click()
-    
+
     cy.get('nav > ul > a').eq(1)
       .click()
 
@@ -30,6 +30,9 @@ describe('Favorite button', () => {
     cy.url().should('eq', 'http://localhost:3000/bookshelf/favorites')
       .get('.bookshelf').should('be.visible')
       .get('.bookCard').should('have.length', 1)
+      .get('.bookshelf > article').eq(0).should('contain', 'THE LAST THING HE TOLD ME')
+        .should('contain', 'Laura Dave')
+        .should('contain', 'Rank: 1')
   })
 })
 
@@ -42,14 +45,14 @@ describe('Favorites display', () => {
   it('should continue to hold current session favorites after returning Home', () => {
     cy.get('.favoritesBtn').eq(0)
       .click()
-    
+
     cy.get('nav > ul > a').eq(0)
       .click()
 
     cy.url().should('eq', 'http://localhost:3000/')
       .get('nav > ul > a').eq(1)
       .click()
-    
+
     cy.url().should('eq', 'http://localhost:3000/bookshelf/favorites')
       .get('.bookshelf').should('be.visible')
       .get('.bookCard').should('have.length', 1)
