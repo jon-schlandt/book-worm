ecf208205d75e15e204bcf2942a7e899f72dfc83
Sarah Lane <sarahlane8@gmail.com>
2021-06-03 13:42:41 -0600

    Refactor should show list of book types test and add className to loading message in App

--- cypress/integration/dashboard_spec.js
+++ cypress/integration/dashboard_spec.js
@@ -3,27 +3,24 @@ describe('Home Page', () => {
       cy.load()
     });
     it('Should show NavBar', () => {
-      cy.get('h2').should('be.visible')
-        .get('h2').contains('Loading...')
-        .get('.nav').should('be.visible')
+      cy.get('.nav').should('be.visible')
         .get('.nav>h1').contains('BookWorm')
-        .get('.nav>ul>li').should('be.visible')
-        .get('.nav>ul>li').eq(0).should('contain', 'Home')
-        .get('.nav>ul>li').eq(1).should('contain', 'Favorites')
+        .get('.nav-links').should('be.visible')
+        .get('.nav-links > a').eq(0).should('contain', 'Home')
+        .get('.nav-links > a').eq(1).should('contain', 'Favorites')
         .get('.nav').contains('Home')
         .get('.nav').contains('Favorites')
-        .get('h2').should('not.exist')
     })
 
     it('Should show list of book types', () => {
-      cy.get('h2').should('be.visible')
-        .get('h2').contains('Loading...')
+      cy.get('.loadingMsg').contains('Loading...')
+        .get('.listTitle > h2').should('be.visible')
+        .get('.listTitle > h2').should('be.visible')
         .get('ul').should('be.visible')
-        .get('ul>li').eq(1)
         .get('.listItem').should('be.visible')
         .get('.listItem').eq(0).should('contain', 'Combined Print & E-Book Fiction')
         .get('.listItem').eq(1).should('contain', 'Combined Print & E-Book Nonfiction')
-        .get('h2').should('not.exist')
+        .get('.loadingMsg').should('not.exist')
     })
 
     it('Should not display the list of book types when a list item is clicked', () => {
@@ -32,4 +29,6 @@ describe('Home Page', () => {
         .get('.nav').should('be.visible')
     })
 
+
+      //it should show an error if the fetch returns nothing
 });
