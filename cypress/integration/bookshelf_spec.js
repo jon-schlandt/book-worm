360aa543f7916a741b578440918c334f67f8310c
Sarah Lane <sarahlane8@gmail.com>
2021-06-03 13:17:34 -0600

    Add test for error message if queryName is not valid

--- cypress/integration/bookshelf_spec.js
+++ cypress/integration/bookshelf_spec.js
@@ -17,12 +17,22 @@ describe('BookshelfDisplay', () => {
       .get('.bookshelf > article').eq(0).should('contain', 'THE LAST THING HE TOLD ME')
         .should('contain', 'Laura Dave')
         .should('contain', 'Rank: 1')
+        .should('contain', 'Add to Favorites 🐛')
       .get('.bookshelf > article').eq(1).should('contain', 'SOOLEY')
         .should('contain', 'John Grisham')
         .should('contain', 'Rank: 2')
+        .should('contain', 'Add to Favorites 🐛')
   })
 
   it('Should include the queryName in the URL', () => {
      cy.url().should('eq', 'http://localhost:3000/bookshelf/combined-print-and-e-book-fiction')
   })
+
+  it('Should display an error message if the URL is not found', () => {
+    cy.visit('http://localhost:3000/bookshelf/not-a-real-genre-type')
+      .get('.noMatchError').should('contain', 'Sorry, page not found!')
+      .get('button').should('contain', 'GO TO HOMEPAGE')
+  })
+
+
 })
