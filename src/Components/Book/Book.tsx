ad879c8569d333a355b7becf44eb52aaa9a4a223
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-06-04 13:33:07 -0400

    Update button text based on state

--- src/Components/Book/Book.tsx
+++ src/Components/Book/Book.tsx
@@ -1,21 +1,64 @@
 import './Book.css'
 import React from 'react'
-import { BookProps } from '../../util/types'
+import { BookProps, BookState } from '../../util/types'
 
-const Book = ({ title, author, rank, bookImage, id, handleClick }: BookProps) => {
-  return (
+// const Book = ({ title, author, rank, bookImage, id, handleClick }: BookProps) => {
+//   return (
+//     <article className='bookCard'>
+//           <span>
+//             <p className='rank'>Rank: {rank}</p>
+//             <button className='favoritesBtn' onClick={ () => handleClick(id)}>Add to Favorites 🐛</button>
+//           </span>
+//           <img className='bookImg' src={bookImage} alt='Book Cover'/>
+//           <div className='bookDetails'>
+//             <h2 className='title'>{title}</h2>
+//             <h3 className='author'>{author}</h3>
+//           </div>
+//         </article>
+//   )
+// }
+
+class Book extends React.Component<BookProps, BookState> {
+  state: BookState;
+  constructor(props: BookProps) {
+    super(props)
+    this.state= {
+      favorite: false
+    }
+  }
+
+  onClick = () => {
+    this.updateFavorite()
+  }
+
+  updateFavorite = () => {
+    if(!this.state.favorite) {
+      this.setState({ favorite: true }, () => this.props.handleClick(this.props.id, this.state.favorite))
+    } else {
+      this.setState({ favorite: false }, () => this.props.handleClick(this.props.id, this.state.favorite))
+    }
+  }
+
+
+  render() {
+    let buttonText = 'Add to Favorites 🐛'
+    if(this.state.favorite){
+      buttonText = 'Remove from Favorites'
+    }
+    return(
     <article className='bookCard'>
-          <span>
-            <p className='rank'>Rank: {rank}</p>
-            <button className='favoritesBtn' onClick={ () => handleClick(id)}>Add to Favorites 🐛</button>
-          </span>
-          <img className='bookImg' src={bookImage} alt='Book Cover'/>
-          <div className='bookDetails'>
-            <h2 className='title'>{title}</h2>
-            <h3 className='author'>{author}</h3>
-          </div>
-        </article>
-  )
+        <span>
+          <p className='rank'>Rank: {this.props.rank}</p>
+          <button className='favoritesBtn' onClick={ () => this.onClick()}>{buttonText}</button>
+        </span>
+        <img className='bookImg' src={this.props.bookImage} alt='Book Cover'/>
+        <div className='bookDetails'>
+          <h2 className='title'>{this.props.title}</h2>
+          <h3 className='author'>{this.props.author}</h3>
+        </div>
+      </article>
+    )
+  }
 }
 
 export default Book
