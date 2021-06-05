6ac4684455831fbbef9bfe1371e9ccd6ff4955ab
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-06-05 19:04:47 -0400

    Refactor Details component to display info

--- src/Components/Details/Details.tsx
+++ src/Components/Details/Details.tsx
@@ -1,10 +1,25 @@
 import './Details.css'
-import { DetailsProps } from '../../util/types'
+import { DetailsProps} from '../../util/types'
 
 const Details = (props: DetailsProps) => {
-
+    console.log(props.info);
+    if(Object.keys(props.info).length) {
     return (
-        <h1>Something</h1>
+        <article className='detailsArticle'>
+        <img className='bookImg' src={props.info.bookImage} alt='Book Cover'/>
+            <div className='detailsContainer'>
+                <h1 className='detailsTitle'>{props.info.title}</h1>
+                <p className='detailsAuthor'>By {props.info.author}</p>
+                <p className='detailsPublisher'>Publisher: {props.info.publisher}</p>
+                <p className='detailsDescription'>Description: {props.info.description}</p>
+                <p className='detailsRank'>rank: {props.info.rank}</p>
+                <a className='detailsAmzLink' href={props.info.amazonProductUrl}>Amazon Link</a>
+            </div>
+        </article>
+    )
+    }
+    return (
+        <h1>Page could not load</h1>
     )
 }
 
